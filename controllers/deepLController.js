ce9922396f659ce40e86130b32dffde13d933b7d
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-10-16 15:29:07 +0200

    DeeL Fix

--- controllers/deepLController.js
+++ controllers/deepLController.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 export const textTranslator = async (req, res, next) => {
+  console.log(req.body);
   try {
     const { data } = await axios.post(
       "https://api-free.deepl.com/v2/translate",
-      {
-        text: [req.body.textInput],
-        target_lang: req.body.targetLang || "DE",
-      },
+      // text: [req.body.textInput],
+      // target_lang: req.body.targetLang || "DE",
+      req.body,
       {
         headers: {
-          Authorization: process.env.DEEPL_ACCESS_KEY,
+          "Content-Type": "application/json",
+          Authorization: `DeepL-Auth-Key ${process.env.DEEPL_ACCESS_KEY}`,
         },
       }
     );
